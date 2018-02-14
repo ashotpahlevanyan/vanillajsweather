b7650d8eca8dec856634465ea128b417f17ce72a
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2018-02-15 02:49:01 +0400

    added data, corrected query to load by city name

--- js/index.js
+++ js/index.js
@@ -1,28 +1,30 @@
 let API_KEY = "468f937be26c4d91fe63cc0f4b7c0c12";
 let apiUrl = "http://api.openweathermap.org/data/2.5/";
-let cityId = 616051;
-let url = apiUrl + '/forecast?id=' + cityId + '&units=metric&APPID=' + API_KEY;
+let cityId = 'Washington';
+//let url = apiUrl + '/forecast?id=' + cityId + '&units=metric&APPID=' + API_KEY;
+let url;
 
-var buttonPrimary = document.querySelector('.buttonPrimary');
-var searchForm = document.querySelector('.form');
-var searchInput = document.querySelector('input.search');
-var result = document.querySelector('.result');
+let buttonPrimary = document.querySelector('.buttonPrimary');
+let searchForm = document.querySelector('.form');
+let searchInput = document.querySelector('input.search');
+let result = document.querySelector('.result');
 
 searchForm.addEventListener('submit', function(e) {
 	e.preventDefault();
 	console.log('form submitted with value ' + searchInput.value.trim());
 
-	var request = new XMLHttpRequest();
+	let request = new XMLHttpRequest();
 	request.open('GET', url);
 	request.responseType = 'json';
 	request.onload = function() {
-	  result.textContent = JSON.stringify(request.response);
+		result.textContent = JSON.stringify(request.response);
 	};
 	request.send();
 });
 
-searchInput.addEventListener('change', function(e) {
+searchInput.addEventListener('input', function(e) {
 	cityId = e.target.value.trim();
+	url = apiUrl + '/forecast?q=' + cityId + '&units=metric&APPID=' + API_KEY;
 });
 
 
@@ -30,10 +32,21 @@ buttonPrimary.addEventListener('click', function(){
 	console.log('button clicked');
 });
 
-var weatherData;
+let weatherData;
 
+let cities;
 
-
+document.addEventListener('DOMContentLoaded', function(){
+	let request = new XMLHttpRequest();
+	request.open('GET', '../data/city.list.json');
+	request.responseType = 'json';
+	request.onload = function() {
+		console.log('Hello');
+	  cities = request.response;
+	  console.log(cities[35687]);
+	};
+	request.send();
+});
 
 
 function updateDisplay() {

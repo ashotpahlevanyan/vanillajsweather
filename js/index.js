68ff539ba465653c39cc194d2806de7aecc110b1
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2018-02-23 19:51:07 +0400

    code restructuring

--- js/index.js
+++ js/index.js
@@ -3,247 +3,19 @@
 let API_KEY = "468f937be26c4d91fe63cc0f4b7c0c12";
 let apiUrl = "http://api.openweathermap.org/data/2.5/";
 let units = 'metric'; //imperial, standard
-const OBSOLETE = 30 * 60000; // 30 mins
+const OBSOLETE = 3 * 60000; // 30 mins
 const DB_NAME = 'WeatherDatabase';
 const DB_STORE_NAME = 'cities';
+const DB_VERSION = 3;
+var db;
+
 // let furl = apiUrl + '/forecast?q=' + cityId + '&units=' + units + '&APPID=' + API_KEY;
 // let curl = apiUrl + '/weather?q=' + cityId + '&units=' + units + '&APPID=' + API_KEY;
 
-
-
-var db;
-
-//Local IndexedDB Database
-
-function checkIDBSupport() {
-	window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
-	window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction || {READ_WRITE: "readwrite"}; // This line should only be needed if it is needed to support the object's constants for older browsers
-	window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange;
-
-	if (!window.indexedDB) {
-		window.alert("Your browser doesn't support a stable version of IndexedDB. Some features elliminating HTTP request will not be available.");
-		return false;
-	} else {
-		return true;
-	}
-}
-
-
-// clearObjectStore(DB_STORE_NAME);
-
-
-/// !!!!!!!!!!!! db name
-function openIDB() {
-	if(!checkIDBSupport()) {
-		console.log('IndexedDB not supported.');
-		return;
-	}
-	console.log('openIDB ...');
-	var requestIDB = window.indexedDB.open(DB_NAME);
-
-	requestIDB.onsuccess = function(event) {
-		console.log('Database is opened, we can now store data there!');
-		db = event.target.result;
-		if(db) {
-			db.onerror = function(event) {
-				alert("Database error: " + event.target.errorCode);
-			};
-			console.log('openIDB Done');
-		}
-	}
-
-	requestIDB.onerror = function(event) {
-		console.log('Could not open a database');
-	}
-
-	requestIDB.onupgradeneeded = function(event) {
-		console.log('IDB.upgradeneeded...');
-
-		var store = db.createObjectStore(DB_STORE_NAME, { keyPath: "uniqueId" });
-
-		store.createIndex("uniqueId", "uniqueId", { unique: true });
-		store.createIndex("value", "value", { unique: false });
-	}
-}
-
-function getObjectStore(storeName, mode) {
-	var transaction = db.transaction(storeName, mode);
-	return transaction.objectStore(storeName);
-}
-
-function addDataToDb(weather) {
-	var uniqueId = generateId(weather.city.coord.lat, weather.city.coord.lon);
-	var obj = {'uniqueId' : uniqueId, 'value': weather};
-	console.log(obj);
-	var store = getObjectStore(DB_STORE_NAME, 'readwrite');
-   var requestIDB;
-
-	requestIDB = store.add(obj);
-
-	requestIDB.onsuccess = function (evt) {
-		console.log("Insertion in DB successful");
-	};
-	
-	requestIDB.onerror = function() {
-		console.error("Insertion in DB error", this.error);
-	};
-}
-
-function clearObjectStore(storeName) {
-	var store = getObjectStore(DB_STORE_NAME, 'readwrite');
-	var requestIDB = store.clear();
-
-	requestIDB.onsuccess = function(event) {
-		console.log('database successfully deleted!');
-	}
-	requestIDB.onerror = function(event) {
-		console.error("clearObjectStore:", event.target.errorCode);
-	}
-}
-
-function searchObjectStore(store, lat, lon) {
-	if (typeof store == 'undefined') {
-		store = getObjectStore(DB_STORE_NAME, 'readonly');
-	}
-
-	var requestIDB;
-	var results = [];
-
-	requestIDB = store.openCursor();
-	requestIDB.onsuccess = function(event) {
-		var cursor = event.target.result;
-
-		if(cursor) {
-			console.log('reading cursor data : ', cursor);
-			requestIDB = store.get(cursor.key);
-
-			requestIDB.onsuccess = function(event) {
-				var value = event.target.result;
-
-				if(isUsableId(lat, lon, value.uniqueID)) {
-					results.push(value);
-				}
-			}
-		}
-		cursor.continue();
-	}
-	return results;
-}
-
-function cleanupObjectStore(store) {
-	if (typeof store == 'undefined') {
-		store = getObjectStore(DB_STORE_NAME, 'readwrite');
-	}
-
-	var requestIDB;
-	var results = [];
-
-	requestIDB = store.openCursor();
-	requestIDB.onsuccess = function(event) {
-		var cursor = event.target.result;
-
-		if(cursor) {
-			console.log('reading cursor data : ', cursor);
-			requestIDB = store.get(cursor.key);
-
-			requestIDB.onsuccess = function(event) {
-				var value = event.target.result;
-
-				if(isObsolete(value.uniqueID)) {
-					requestIDB = store.delete(value.uniqueID);
-					requestIDB.onsuccess = function(event) {
-						console.log("obsolete data delete successful");
-					}
-					requestIDB.onerror = function(event) {
-						console.error("delete data:", event.target.errorCode);
-					}
-				}
-			}
-		}
-		cursor.continue();
-	}
-	requestIDB.onerror = function (event) {
-		console.error("cleanup:", event.target.errorCode);
-	};
-}
-
-
-	
-// function createIDB() {
-// 	if(idbSupported) {
-// 		var requestIDB = window.indexedDB.open("WeatherDatabase", 3);
-// 		var db;
-
-// 		requestIDB.onerror = function(event) {
-// 			alert('Could not open a database');
-// 		}
-
-// 		requestIDB.onsuccess = function(event) {
-// 			console.log('Database is opened, we can now store data there!');
-// 			db = event.target.result;
-// 			if(db) {
-// 				db.onerror = function(event) {
-// 					alert("Database error: " + event.target.errorCode);
-// 				};
-// 			}
-// 		}
-
-// 		requestIDB.onupgradeneeded = function(event) { 
-// 			// Save the IDBDatabase interface 
-// 			var db = event.target.result;
-
-// 			// Create an objectStore for this database
-// 			var objectStore = db.createObjectStore("cities", { keyPath: "uniqueId" });
-
-// 			objectStore.createIndex("uniqueId", "uniqueId", { unique: true });
-// 			objectStore.createIndex("name", "name", { unique: false });
-// 			objectStore.createIndex("latlng", "latlng", { unique: false });
-// 			objectStore.createIndex("value", "value", { unique: false });
-
-// 			objectStore.transaction.oncomplete = function(event) {
-// 				// Store values in the newly created objectStore.
-// 				var cityObjectStore = db.transaction("cities", "readwrite").objectStore("cities");
-// 				cityData.forEach(function(city) {
-// 					cityObjectStore.add(city);
-// 				});
-// 			};
-
-// 		};
-// 	}
-// }
-// Let us open our database
-
-// createIDB(checkIDBSupport());
-
-
-
-
-//logic for idb storage
-
-/*
-on load, check if the location city is there
-	1. open the database, if not exist, 
-		create one and go to load weather by coordinates
-	2. if the database is open, check if the existing data contain 
-		a key with current lat-lng or cityID (key will be an object)
-	3. if database contains the existing data, 
-		and timestamp in db is in allowed range,
-		then load the data from the locat db
-	4. else, delet the current db record, make a XHR request
-		after request is done, store the result in db with 
-		current timestamp and cityID
-*/ 
-
-//stamp can be a string with date-time + ::cityID
-
-
 // Storage variables
 
-
-
-
-// let currentWeather;
-// let forecastWeather;
+let currentWeather;
+let forecastWeather;
 
 // DOM elements
 let weatherWrapper = document.querySelector('.weatherWrapper');
@@ -257,7 +29,6 @@ document.addEventListener('DOMContentLoaded', domContentLoaded);
 document.addEventListener('DOMContentLoaded', showHideWrapper);
 
 function domContentLoaded() {
-	openIDB();
 	navigator.geolocation.getCurrentPosition(weatherByPosition);
 }
 
@@ -315,7 +86,6 @@ function XHRCall(url, type, cb) {
 	request.send();
 }
 
-
 /*
 	Display Functions
 */
@@ -326,6 +96,7 @@ function displayCurrent(weather) {
 }
 
 function displayForecast(weather) {
+	forecastWeather = weather;
 	console.log(weather);
 	//result.textContent = JSON.stringify(weather);
 	enhanceWeather(weather);
@@ -342,7 +113,6 @@ function showHideWrapper() {
 		console.log('block');
 	} else {
 		weatherWrapper.style.display = 'none';
-		console.log('none');
 	}
 }
 
@@ -378,7 +148,6 @@ function enhanceWeather(weather) {
 	console.log(weather);
 }
 
-
 function updateCurrent(weather){
 	if(!weather) {
 		return;
@@ -389,34 +158,45 @@ function updateCurrent(weather){
 	}
 	var current = document.createElement('div');
 	current.className = 'current';
+
 	var currentHeader = document.createElement('h4');
 	currentHeader.classList = ['currentHeader', 'clearfix'].join(' ');
-	var	currentDate = new Date();
+
+	var currentDate = new Date();
 	var day = document.createElement('span');
 	day.classList = ['day', 'float-left'].join(' ');
 	day.textContent = dateNameByValue('day', currentDate.getDay());
+
 	var date = document.createElement('span');
 	date.classList = ['day', 'float-right'].join(' ');
 	date.textContent = currentDate.getDate() + ' ' + dateNameByValue('month', currentDate.getMonth());
+
 	current.appendChild(currentHeader);
 	currentHeader.appendChild(day);
 	currentHeader.appendChild(date);
 
 	var content = document.createElement('div');
 	content.className = 'content';
+
 	var location = document.createElement('div');
 	location.className = 'location';
 	location.textContent = weather.name;
+
 	var weatherData = document.createElement('div');
 	weatherData.className = 'weatherData';
+
 	content.appendChild(location);
 	content.appendChild(weatherData);
+
 	var temperature = document.createElement('div');
 	temperature.className = 'temperature';
+
 	var tempValue = document.createElement('span');
 	tempValue.className = 'value';
 	tempValue.textContent = weather.main.temp;
+
 	temperature.appendChild(tempValue);
+
 	var unit = document.createElement('span');
 	unit.className = 'unit';
 	if(units == 'metric') {
@@ -427,20 +207,26 @@ function updateCurrent(weather){
 	} else {
 		unit.textContent = 'F';
 	}
+
 	temperature.appendChild(unit);
+
 	var sky = document.createElement('div');
 	sky.className = 'sky';
+
 	var skyIcon = document.createElement('i');
 	skyIcon.classList = calculateSkyClass(weather.weather[0].icon);
+
 	sky.appendChild(skyIcon);
 	weatherData.appendChild(temperature);
 	weatherData.appendChild(sky);
 
 	var otherData = document.createElement('ul');
 	otherData.className = 'otherData';
+
 	var humidity = document.createElement('li');
 	var umbrella = document.createElement('i')
 	umbrella.classList = ['wi', 'wi-umbrella'].join(' ');
+
 	humidity.appendChild(umbrella);
 	humidity.appendChild(document.createTextNode(weather.main.humidity));
 	humidity.appendChild(document.createTextNode('%'));
@@ -448,6 +234,7 @@ function updateCurrent(weather){
 	var wind = document.createElement('li');
 	var windIcon = document.createElement('i')
 	windIcon.classList = ['wi', 'wi-strong-wind'].join(' ');
+
 	wind.appendChild(windIcon);
 	wind.appendChild(document.createTextNode(weather.wind.speed));
 	wind.appendChild(document.createTextNode('m/s'));
@@ -455,6 +242,7 @@ function updateCurrent(weather){
 	var direction = document.createElement('li');
 	var directionIcon = document.createElement('i')
 	directionIcon.className = 'direction ' + calculateDirectionClass(weather.wind.deg);
+
 	direction.appendChild(directionIcon);
 	direction.appendChild(document.createTextNode(calculateDirectionText(weather.wind.deg)));
 
@@ -464,6 +252,7 @@ function updateCurrent(weather){
 
 	content.appendChild(otherData);
 	current.appendChild(content);
+
 	content.style.backgroundImage = "url('" + calculateImageUrl(weather.weather[0].icon) + "')";
 
 	if(weatherWrapper.firstChild) {
@@ -608,6 +397,214 @@ function createCell(item) {
 
 	return td;
 }
+
+
+/**
+ * IndexedDB utility functions
+ *
+ * checkIDBSupport()
+ * 
+ *
+ * 
+ */
+
+
+//Local IndexedDB Database
+
+function checkIDBSupport() {
+	window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+	window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction || {READ_WRITE: "readwrite"}; // This line should only be needed if it is needed to support the object's constants for older browsers
+	window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange;
+
+	if (!window.indexedDB) {
+		window.alert("Your browser doesn't support a stable version of IndexedDB. Some features elliminating HTTP request will not be available.");
+		return false;
+	} else {
+		return true;
+	}
+}
+
+
+function openIDB() {
+	if(!checkIDBSupport()) {
+		return;
+	}
+	console.log('openIDB ...');
+	var req = indexedDB.open(DB_NAME, DB_VERSION);
+	req.onsuccess = function (evt) {
+		db = this.result;
+		console.log("openDb DONE");
+	};
+	req.onerror = function (evt) {
+		console.error("openDb:", evt.target.errorCode);
+	};
+
+	req.onupgradeneeded = function (evt) {
+		console.log("openDb.onupgradeneeded");
+		var store = evt.currentTarget.result.createObjectStore(
+	  		DB_STORE_NAME, { keyPath: 'uniqueId'});
+
+		store.createIndex("uniqueId", "uniqueId", { unique: true });
+		store.createIndex("value", "value", { unique: false });
+	}
+}
+
+function getObjectStore(storeName, mode) {
+	var transaction = db.transaction(storeName, mode);
+	return transaction.objectStore(storeName);
+}
+
+function addDataToDb(weather) {
+	var uniqueId = generateId(weather.city.coord.lat, weather.city.coord.lon);
+	var obj = {'uniqueId' : uniqueId, 'value': weather};
+	console.log(obj);
+	var store = getObjectStore(DB_STORE_NAME, 'readwrite');
+	if(!store) {
+		console.log('could not get an object store, cannot add data to db');
+	}
+   var requestIDB;
+
+	requestIDB = store.add(obj);
+
+	requestIDB.onsuccess = function (evt) {
+		console.log("Insertion in DB successful");
+	};
+	
+	requestIDB.onerror = function() {
+		console.error("Insertion in DB error", this.error);
+	};
+}
+
+function clearObjectStore(storeName) {
+	var store = getObjectStore(DB_STORE_NAME, 'readwrite');
+	var requestIDB = store.clear();
+
+	requestIDB.onsuccess = function(event) {
+		console.log('database successfully deleted!');
+	}
+	requestIDB.onerror = function(event) {
+		console.error("clearObjectStore:", event.target.errorCode);
+	}
+}
+
+function searchObjectStore(store, lat, lon) {
+	if (typeof store == 'undefined') {
+		store = getObjectStore(DB_STORE_NAME, 'readonly');
+	}
+
+	var requestIDB;
+	var results = [];
+
+	requestIDB = store.openCursor();
+	requestIDB.onsuccess = function(event) {
+		var cursor = event.target.result;
+
+		if(cursor) {
+			console.log('reading cursor data : ', cursor);
+			requestIDB = store.get(cursor.key);
+
+			requestIDB.onsuccess = function(event) {
+				var value = event.target.result;
+
+				if(isUsableId(lat, lon, value.uniqueID)) {
+					results.push(value);
+				}
+			}
+		}
+		cursor.continue();
+	}
+	return results;
+}
+
+function cleanupObjectStore(store) {
+	if (typeof store == 'undefined') {
+		store = getObjectStore(DB_STORE_NAME, 'readwrite');
+	}
+
+	var requestIDB;
+	var results = [];
+
+	requestIDB = store.openCursor();
+	requestIDB.onsuccess = function(event) {
+		var cursor = event.target.result;
+
+		if(cursor) {
+			console.log('reading cursor data : ', cursor);
+			requestIDB = store.get(cursor.key);
+
+			requestIDB.onsuccess = function(event) {
+				var value = event.target.result;
+
+				if(isObsolete(value.uniqueID)) {
+					requestIDB = store.delete(value.uniqueID);
+					requestIDB.onsuccess = function(event) {
+						console.log("obsolete data delete successful");
+					}
+					requestIDB.onerror = function(event) {
+						console.error("delete data:", event.target.errorCode);
+					}
+				}
+			}
+		}
+		cursor.continue();
+	}
+	requestIDB.onerror = function (event) {
+		console.error("cleanup:", event.target.errorCode);
+	};
+}
+
+
+	
+// function createIDB() {
+// 	if(idbSupported) {
+// 		var requestIDB = window.indexedDB.open("WeatherDatabase", 3);
+// 		var db;
+
+// 		requestIDB.onerror = function(event) {
+// 			alert('Could not open a database');
+// 		}
+
+// 		requestIDB.onsuccess = function(event) {
+// 			console.log('Database is opened, we can now store data there!');
+// 			db = event.target.result;
+// 			if(db) {
+// 				db.onerror = function(event) {
+// 					alert("Database error: " + event.target.errorCode);
+// 				};
+// 			}
+// 		}
+
+// 		requestIDB.onupgradeneeded = function(event) { 
+// 			// Save the IDBDatabase interface 
+// 			var db = event.target.result;
+
+// 			// Create an objectStore for this database
+// 			var objectStore = db.createObjectStore("cities", { keyPath: "uniqueId" });
+
+// 			objectStore.createIndex("uniqueId", "uniqueId", { unique: true });
+// 			objectStore.createIndex("name", "name", { unique: false });
+// 			objectStore.createIndex("latlng", "latlng", { unique: false });
+// 			objectStore.createIndex("value", "value", { unique: false });
+
+// 			objectStore.transaction.oncomplete = function(event) {
+// 				// Store values in the newly created objectStore.
+// 				var cityObjectStore = db.transaction("cities", "readwrite").objectStore("cities");
+// 				cityData.forEach(function(city) {
+// 					cityObjectStore.add(city);
+// 				});
+// 			};
+
+// 		};
+// 	}
+// }
+// Let us open our database
+
+// createIDB(checkIDBSupport());
+
+
+
+
+
 
 /*
 	Utility functions
@@ -892,4 +889,6 @@ function isUsableId(lat, lon, oldId) {
 }
 
 
+openIDB();
 
+clearObjectStore();
